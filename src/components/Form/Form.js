892e2b4bb3245a4f4e355ea259f1db1d7cc43f12
Ellen Probst <ellenprobst@hotmail.com>
2018-06-27 18:17:22 -0400

    Small ui updates

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -33,12 +33,10 @@ class Form extends Component {
 		})
 	}
 
-	handleSelect = name => (event, value) => {
-		this.setState({ [name]: value })
-	}
-
 	handleSwitch = name => event => {
-		this.setState({ [name]: event.target.checked })
+		this.setState({
+			[name]: event.target
+		})
 	}
 
 	handleSubmit = e => {
@@ -54,21 +52,28 @@ class Form extends Component {
 				<label htmlFor="title">Title:</label>
 				<input name="title" type="text" placeholder="title" onChange={this.handleChange('title')} />
 				<label htmlFor="size">Size:</label>
-				<input name="size" type="range" min="1" max="5" defaultValue="3" onChange={this.handleSelect('size')} />
+				<input name="size" type="range" min="1" max="5" defaultValue="3" onChange={this.handleChange('size')} />
 				<label htmlFor="colour">Colour:</label>
-				<input name="colour" type="range" min="1" max="5" defaultValue="3" onChange={this.handleSelect('colour')} />
+				<input name="colour" type="range" min="1" max="5" defaultValue="3" onChange={this.handleChange('colour')} />
 				<label htmlFor="geometry">sphere:</label>
-				<input name="geometry" type="radio" />
+				<input
+					name="geometry"
+					type="radio"
+					value="sphere"
+					defaultChecked={false}
+					onChange={() => this.handleSwitch('geometry')}
+				/>
 				<label htmlFor="geometry">cube:</label>
-				<input name="geometry" type="radio" />
+				<input name="geometry" type="radio" value="cube" onChange={() => this.handleSwitch('geometry')} />
 				<label htmlFor="geometry">torus:</label>
-				<input name="geometry" type="radio" />
-				<label htmlFor="geometry">animate:</label>
-				<input name="animate" type="checkbox" />
+				<input name="geometry" type="radio" value="torus" onChange={() => this.handleSwitch('geometry')} />
+				<label htmlFor="animate">animate:</label>
+				<input name="animate" type="checkbox" value="sphere" onChange={() => this.handleSwitch('isAnimated')} />
 				<label htmlFor="wireframe">wireframe:</label>
-				<input name="wireframe" type="checkbox" />
+				<input type="checkbox" onChange={e => this.handleSwitch('isWireframe')} />
 				<label htmlFor="children">children:</label>
-				<input name="children" type="checkbox" />
+				<input name="children" type="checkbox" value="isWireframe" onChange={() => this.handleSwitch('hasDhildren')} />
+				<input type="checkbox" onChange={this.handleSwitch} />
 				<button type="submit" className="button">
 					save
 				</button>
