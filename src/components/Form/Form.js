df8921ce11c439af7a773ccdbd45a039640d634c
Ellen Probst <ellenprobst@hotmail.com>
2018-07-02 13:54:33 -0400

    clean up form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react'
 import './form.css'
 
 class Form extends Component {
-	handleChange = title => event => this.props.updateState(title, event.target.value)
-	handleSwitch = name => event => this.props.updateState(name, event.target.checked)
+	handleChange = event => {
+		const target = event.target
+		const value = target.type === 'checkbox' ? target.checked : target.value
+		const name = target.name
+
+		this.props.updateState(name, value)
+	}
 
 	handleSubmit = e => e.preventDefault()
 
@@ -14,47 +19,30 @@ class Form extends Component {
 			<form autoComplete="off" onSubmit={this.handleSubmit} className="form">
 				<label htmlFor="title">
 					Title:
-					<input name="title" type="text" placeholder={title} onChange={this.handleChange('title')} />
+					<input name="title" type="text" placeholder={title} onChange={this.handleChange} />
 				</label>
 
 				<label htmlFor="size">Size:</label>
-				<input name="size" type="range" min="1" max="5" defaultValue={size} onChange={this.handleChange('size')} />
+				<input name="size" type="range" min="1" max="5" defaultValue={size} onChange={this.handleChange} />
 				<label htmlFor="colour">Colour:</label>
-				<input name="colour" type="range" min="1" max="5" defaultValue={size} onChange={this.handleChange('colour')} />
+				<input name="colour" type="range" min="1" max="5" defaultValue={size} onChange={this.handleChange} />
 				<label htmlFor="geometry">sphere:</label>
-				<input
-					name="geometry"
-					type="radio"
-					value="sphere"
-					defaultChecked={false}
-					onChange={() => this.handleSwitch('geometry')}
-				/>
+				<input name="geometry" type="radio" value="sphere" defaultChecked={false} onChange={() => this.handleChange} />
 				<label htmlFor="geometry">cube:</label>
-				<input name="geometry" type="radio" value="cube" onChange={() => this.handleSwitch('geometry')} />
+				<input name="geometry" type="radio" value="cube" onChange={() => this.handleChange} />
 				<label htmlFor="geometry">torus:</label>
-				<input name="geometry" type="radio" value="torus" onChange={() => this.handleSwitch('geometry')} />
+				<input name="geometry" type="radio" value="torus" onChange={() => this.handleChange} />
 				<label htmlFor="animate">animate:</label>
-				<input name="animate" type="checkbox" value="sphere" onChange={() => this.handleSwitch('isAnimated')} />
+				<input name="animate" type="checkbox" value="sphere" onChange={() => this.handleChange} />
 				<label>
 					wireframe:
-					<input
-						name="isWireframe"
-						type="checkbox"
-						defaultChecked={isWireframe}
-						onChange={this.handleSwitch('isWireframe')}
-					/>
+					<input name="isWireframe" type="checkbox" defaultChecked={isWireframe} onChange={this.handleChange} />
 				</label>
 				<label>
 					children:
-					<input
-						name="hasChildren"
-						type="checkbox"
-						defaultChecked={hasChildren}
-						onChange={this.handleSwitch('hasChildren')}
-					/>
+					<input name="hasChildren" type="checkbox" defaultChecked={hasChildren} onChange={this.handleChange} />
 				</label>
 
-				<input type="checkbox" onChange={this.handleSwitch} />
 				<button type="submit" className="button">
 					save
 				</button>
